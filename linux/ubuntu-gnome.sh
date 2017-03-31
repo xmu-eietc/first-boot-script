cd812ca1176e037422ffd55e071fc51004832ec8
Wind Wood <windwood@gmail.com>
2017-03-31 20:49:38 +0800

    append tweak setting
    增加系统优化设置

--- linux/ubuntu-gnome.sh
+++ linux/ubuntu-gnome.sh
@@ -4,3 +4,15 @@ apt update
 apt full-upgrade -y
 apt install -y aptitude apt-file && apt-file update
 apt install -y vim git
+
+# 优化工具
+## 启用扩展
+### Applications menu
+### Places status indicator
+### Window list
+## Desktop
+### Desktop Icon: 主文件夹、回收站、已挂载的卷
+## 窗口
+### 标题栏按钮: 打开最大化、最小化
+## 顶栏
+### 时钟: 显示日期
